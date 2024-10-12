a91db45461a49b6eeb837e7d46d72b1bb1a5dd6a
Jack Zheng <jack.zheng.nz@gmail.com>
2024-10-13 09:49:22 +1100

    Changed long data's dates

--- data/convertToLong.py
+++ data/convertToLong.py
@@ -4,6 +4,9 @@ import pandas as pd
 file_path = 'EmploymentInformationWide.csv'
 employment_info_wide = pd.read_csv(file_path)
 
+# Convert the 'Date' column to a proper date format
+employment_info_wide['Date'] = pd.to_datetime(employment_info_wide['Date'], format='%b-%Y')
+
 # Melt the data to convert it into long format
 employment_info_long = pd.melt(employment_info_wide, id_vars=["Date"], 
                                var_name="Metric", value_name="Value")
